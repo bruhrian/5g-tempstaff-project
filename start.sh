6806068bd80c00e85bdc7a85fb9b81fc1128a3be
Brian Chua <brianchuayx@gmail.com>
2026-05-05 09:40:29 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! Use git bash to run this script using 'bash start.sh'
 source .venv/Scripts/activate
 cd backend && uvicorn session_app.main:app --reload &
 cd frontend/my-app && npm run dev
